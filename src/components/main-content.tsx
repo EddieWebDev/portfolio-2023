a6469bdd8e2c056bf8c7cdb199bbbf77ccd7d636
Eddie Andersson <eddandersson@gmail.com>
2023-05-08 00:43:35 +0200

    fixed image width and github links

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -8,12 +8,12 @@ export const MainContent = () => {
         Featured projects
       </h2>
       <ul className="mx-auto my-12 flex list-none flex-col items-center gap-8 lg:flex-row">
-        <li className="dark:border-grey-100 flex w-2/3 flex-col items-center rounded-3xl border border-solid border-slate-900 bg-white px-2 py-6 shadow-xl dark:bg-black sm:w-5/6">
+        <li className="dark:border-grey-100 flex w-2/3 flex-col items-center rounded-3xl border border-solid border-slate-900 bg-white px-2 py-6 shadow-xl dark:bg-black">
           <a href="https://eddiewebshop.com" target="_blank">
             <img
               src="/img/fruitcity2.png"
               alt="eddiewebshop"
-              className="my-3 lg:max-h-32"
+              className="mx-auto my-3 lg:max-h-32"
             />
             <h3 className="text-center text-3xl text-slate-900 dark:text-white">
               Web Shop JS
@@ -37,6 +37,12 @@ export const MainContent = () => {
                 API
               </li>
             </ul>
+            <a
+              href="https://github.com/EddieWebDev/eddie-exam-shop"
+              target="_blank"
+            >
+              <div className="rounded-xl border border-primary p-1 hover:bg-primary mt-2">Github</div>
+            </a>
           </div>
           <div className="mt-2 text-center text-slate-400 dark:text-white sm:hidden">
             <ul className="flex flex-wrap justify-center gap-1 text-sm">
@@ -56,10 +62,16 @@ export const MainContent = () => {
                 API
               </li>
             </ul>
+            <a
+              href="https://github.com/EddieWebDev/eddie-exam-shop"
+              target="_blank"
+            >
+              <div className="rounded-xl border border-primary p-1 hover:bg-primary mt-2">Github</div>
+            </a>
           </div>
         </li>
 
-        <li className="dark:border-grey-100 flex w-2/3 flex-col items-center rounded-3xl border border-solid border-slate-900 bg-white px-2 py-6 shadow-xl dark:bg-black sm:w-5/6">
+        <li className="dark:border-grey-100 flex w-2/3 flex-col items-center rounded-3xl border border-solid border-slate-900 bg-white px-2 py-6 shadow-xl dark:bg-black">
           <a
             href="https://github.com/EddieWebDev/portfolio-2023"
             target="_blank"
@@ -67,7 +79,7 @@ export const MainContent = () => {
             <img
               src="/img/rocketride.png"
               alt="Adventurer"
-              className="mb-6 lg:max-h-32"
+              className="mx-auto mb-6 lg:max-h-32"
             />
             <h3 className="text-center text-3xl text-slate-900 dark:text-white">
               Portfolio
@@ -88,6 +100,12 @@ export const MainContent = () => {
                 Typescript
               </li>
             </ul>
+            <a
+              href="https://github.com/EddieWebDev/portfolio-2023"
+              target="_blank"
+            >
+              <div className="rounded-xl border border-primary p-1 hover:bg-primary mt-2">Github</div>
+            </a>
           </div>
           <div className="mt-2 text-center text-slate-400 dark:text-white sm:hidden">
             <ul className="flex flex-wrap justify-center gap-1 text-sm">
@@ -104,15 +122,21 @@ export const MainContent = () => {
                 Typescript
               </li>
             </ul>
+            <a
+              href="https://github.com/EddieWebDev/portfolio-2023"
+              target="_blank"
+            >
+              <div className="rounded-xl border border-primary p-1 hover:bg-primary mt-2">Github</div>
+            </a>
           </div>
         </li>
 
-        <li className="dark:border-grey-100 flex w-2/3 flex-col items-center rounded-3xl border border-solid border-slate-900 bg-white px-2 py-6 shadow-xl dark:bg-black sm:w-5/6">
+        <li className="dark:border-grey-100 flex w-2/3 flex-col items-center rounded-3xl border border-solid border-slate-900 bg-white px-2 py-6 shadow-xl dark:bg-black">
           <a href="https://github.com/EddieWebDev/WCDota" target="_blank">
             <img
               src="/img/rocketlaunch.png"
               alt="Infinity"
-              className="mb-6 lg:max-h-32"
+              className="mx-auto mb-6 lg:max-h-32"
             />
             <h3 className="text-center text-3xl text-slate-900 dark:text-white">
               Web Shop PHP
@@ -130,6 +154,12 @@ export const MainContent = () => {
                 Woocommerce
               </li>
             </ul>
+            <a
+              href="https://github.com/EddieWebDev/WCDota"
+              target="_blank"
+            >
+              <div className="rounded-xl border border-primary p-1 hover:bg-primary mt-2">Github</div>
+            </a>
           </div>
           <div className="mt-2 text-center text-slate-400 dark:text-white sm:hidden">
             <ul className="flex flex-wrap justify-center gap-1 text-sm">
@@ -143,9 +173,16 @@ export const MainContent = () => {
                 Node
               </li>
             </ul>
+            <a
+              href="https://github.com/EddieWebDev/WCDota"
+              target="_blank"
+            >
+              <div className="rounded-xl border border-primary p-1 hover:bg-primary mt-2">Github</div>
+            </a>
           </div>
         </li>
       </ul>
+      
       <p className="text-md my-6 text-center font-bold text-slate-900 dark:text-white sm:text-xl">
         More projects coming soon!
       </p>
